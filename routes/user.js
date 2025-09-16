34e1b1ded82e9947153504007d271b0489e28a9c
Piyush707-DS <rathorepiyush787@gmail.com>
2025-09-16 18:12:12 +0530

    all commits

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,12 @@ const userController = require('../controllers/userController');
 const GMem = require('../models/groupMem.model');
 
 router.get('/dashboard',authMiddleware,userController.dashboard);
-
+router.get('/Announcement',authMiddleware,userController.Announcement);
 router.post('/update-profile',authMiddleware, userController.updateProfile);
 
 router.get('/user/:id', authMiddleware, isAdmin, userController.getMemberDetails);
 router.post('/user/:id/verify', authMiddleware, isAdmin, userController.verifyMember);
 
 router.get('/notifs',authMiddleware,userController.notifications);
-
+router.get('/Announcementform',authMiddleware,userController.Announcementform);
 module.exports = router;
