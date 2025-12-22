e3bed2a7d945249f0c81a74fa72b7a146a6c61c0
Nayaknaina <nainanayak288@gmail.com>
2025-12-22 14:04:11 +0530

    profile image  ,and scroller issue resolves

--- middleware/profileImageMiddleware.js
+++ middleware/profileImageMiddleware.js
@@ -6,24 +6,30 @@ const profileImageMiddleware = async (req, res, next) => {
   try {
 
     if (req.user) {
-      const user = await User.findById(req.user.id).select('profilePicture f_name');
+      const user = await User.findById(req.user.id).select('profilePicture f_name googleId').lean();
 
       if (user) {
        let profilePictureUrl = '/assets/images/default-avatar.png'; 
 
-        if (
-          user.profilePicture &&
-          !user.profilePicture.startsWith('assets/images/default-avatar.png') &&
-          !user.profilePicture.startsWith('/assets/images/default-avatar.png')
-        ) {
-          try {
-            profilePictureUrl = await getSignedUrl(user.profilePicture);
-            console.log('Generated signed URL for profile picture:', profilePictureUrl);
-          } catch (err) {
-            console.error('Error generating signed URL for profile picture:', err);
-          }
-        }
+            if (user.googleId && user.profilePicture) {
+            profilePictureUrl = user.profilePicture;
+                      console.log('Initial profile picture URL:', profilePictureUrl);
 
+          }
+            else if (
+              user.profilePicture &&
+              !user.profilePicture.startsWith('assets/images/default-avatar.png') &&
+              !user.profilePicture.startsWith('/assets/images/default-avatar.png')
+            ) {
+              console.log('Generating signed URL for profile picture:', user.profilePicture);
+              try {
+                profilePictureUrl = await getSignedUrl(user.profilePicture);
+                console.log('Generated signed URL for profile picture:', profilePictureUrl);
+              } catch (err) {
+                console.error('Error generating signed URL for profile picture:', err);
+              }
+            }
+            console.log('Using profile picture URL:', profilePictureUrl);
         res.locals.userProfile = {
           name: user.f_name,
           profilePicture: profilePictureUrl,
